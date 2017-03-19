876b2229a2fa7a4f394dab9a6bfe4ba633c42aa4
YourBoB <zhaojunbao123@163.com>
2017-03-19 22:25:23 +1300

    show datas from database

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from . import models
 def index(request):
    #return HttpResponse('hello world')
-    return render(request,'index.html',{'hello':'hello blog!!!'})
+   article= models.Article.objects.all()
+   return render(request,'index.html',{'hello':'hello blog!!!','article':article})
