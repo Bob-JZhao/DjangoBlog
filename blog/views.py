ccbedd7418356d75d76e22d06b4f8f99f785cdec
YourBoB <zhaojunbao123@163.com>
2017-03-27 15:32:10 +1300

    create article

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,8 @@ def index(request):
    #return HttpResponse('hello world')
    article= models.Article.objects.all()
    return render(request,'index.html',{'hello':'hello blog!!!','article':article})
+def article_page(request,article_id):
+   article = models.Article.objects.get(pk=article_id)
+   return render(request,'article_page.html',{'article': article})
+def new(request):
+   return  render(request,'new.html')
