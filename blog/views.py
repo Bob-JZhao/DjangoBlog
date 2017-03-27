a9f59b1d323f39e92bd65f13ff80d6e661608b87
YourBoB <zhaojunbao123@163.com>
2017-03-28 12:22:39 +1300

    new,edite article

--- blog/views.py
+++ blog/views.py
@@ -9,5 +9,22 @@ def index(request):
 def article_page(request,article_id):
    article = models.Article.objects.get(pk=article_id)
    return render(request,'article_page.html',{'article': article})
-def new(request):
+def new_page(request):
    return  render(request,'new.html')
+def edit_page(request,article_id):
+   article = models.Article.objects.get(pk=article_id)
+   return render(request,'edit.html',{'article':article})
+def save(request):
+   title= request.POST.get('title','title')
+   content=request.POST.get('content','content')
+   id=request.POST.get('id','')
+   if id !='':
+      models.Article.objects.update_or_create(pk=id,defaults={'title':title,'content':content})
+      print('do update')
+   else:
+      models.Article.objects.create(title=title,content=content)
+   article = models.Article.objects.all()
+   return render(request, 'index.html', {'hello': 'hello blog!!!', 'article': article})
+
+
+
